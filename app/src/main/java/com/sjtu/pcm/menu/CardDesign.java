4940901f92038ae3cbb1eefb119380edd870e39e
SeniYuting <2580238659@qq.com>
2016-12-16 16:31:52 +0800

    二维码功能

--- app/src/main/java/com/sjtu/pcm/menu/CardDesign.java
+++ app/src/main/java/com/sjtu/pcm/menu/CardDesign.java
@@ -4,15 +4,19 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.google.zxing.WriterException;
 import com.sjtu.pcm.MyApplication;
 import com.sjtu.pcm.R;
 import com.sjtu.pcm.activity.card_design.CardModifyActivity;
@@ -20,9 +24,7 @@ import com.sjtu.pcm.anim.MyViewGroup.OnOpenListener;
 import com.sjtu.pcm.entity.CardEntity;
 import com.sjtu.pcm.entity.CardList;
 import com.sjtu.pcm.util.HttpUtil;
-
-import java.util.ArrayList;
-import java.util.HashMap;
+import com.sjtu.pcm.zxing.EncodingHandler;
 
 /**
  * 名片设计类
@@ -45,6 +47,11 @@ public class CardDesign {
 	private TextView cCAddress;
 	private TextView cFax;
 	private TextView cCEmail;
+
+	private Button cQrCodeBtn;
+	private ImageView qrImgImageView;
+
+	private String contentString;
 
 	private MyApplication mApp;
 	private OnOpenListener mOnOpenListener;
@@ -81,6 +88,9 @@ public class CardDesign {
 		cCAddress = (TextView) mHome.findViewById(R.id.card_c_address);
 		cFax = (TextView) mHome.findViewById(R.id.card_fax);
 		cCEmail = (TextView) mHome.findViewById(R.id.card_c_email);
+
+		cQrCodeBtn = (Button) mHome.findViewById(R.id.qr_code);
+		qrImgImageView = (ImageView) mHome.findViewById(R.id.qr_image);
 	}
 
 	/**
@@ -105,6 +115,27 @@ public class CardDesign {
 				intent.setClass(cDContext, CardModifyActivity.class);
 				cDContext.startActivity(intent);
 			}
+		});
+
+		// 生成二维码监听
+		cQrCodeBtn.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				try {
+					if (!contentString.equals("")) {
+						//根据字符串生成二维码图片并显示在界面上，第二个参数为图片的大小（350*350）
+						Bitmap qrCodeBitmap = EncodingHandler.createQRCode(contentString, 350);
+						qrImgImageView.setImageBitmap(qrCodeBitmap);
+
+					}else {
+						Toast.makeText(cDContext, "Text can not be empty", Toast.LENGTH_SHORT).show();
+					}
+
+				} catch (WriterException e) {
+					e.printStackTrace();
+				}
+			}
+
 		});
 	}
 
@@ -167,6 +198,15 @@ public class CardDesign {
 			cCAddress.setText(cardEntity.getC_address());
 			cFax.setText(cardEntity.getC_fax());
 			cCEmail.setText(cardEntity.getC_email());
+
+			contentString = "名片\n"
+					+ "姓名: " + cardEntity.getName() + "\n"
+					+ "公司: " + cardEntity.getCompany() + "\n"
+					+ "职位: " + cardEntity.getJob() + "\n"
+					+ "公司电话: " + cardEntity.getC_number() + "\n"
+					+ "公司地址: " + cardEntity.getC_address() + "\n"
+					+ "传真:" + cardEntity.getC_fax() + "\n"
+			   		+ "公司邮箱: " + cardEntity.getC_email() + "\n";
 		}
 
 	}
